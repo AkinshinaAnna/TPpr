99ad25250309c06b25e748c194aa4d71be5c5402
AkinshinaAS <62110748+AkinshinaAS@users.noreply.github.com>
2020-04-22 22:40:53 +0300

    Initial commit

---
+++ src/main/java/app/service/AbstractUserService.java
@@ -0,0 +1,23 @@
+package app.service;
+
+import app.domain.Customer;
+
+import java.util.List;
+
+public abstract class AbstractUserService {
+
+    abstract public boolean registration(String nameCompany, String login, String password);
+
+    abstract public Long getIdByLogin(String login);
+
+    abstract public boolean authorization(String login, String password);
+
+
+
+    abstract public void delete(Long id);
+    abstract  public void change(Long id, String nameCompany, String login, String password, String aboutCompany, String aboutPro, String contacts);
+
+
+
+
+}
